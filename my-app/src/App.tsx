c7dd94b0f2d229c08db30812a7faf86f1db95607
Cycbloom <1756138655@qq.com>
2025-02-22 00:03:51 +0800

    Routing

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,7 +1,15 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello,world!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 }
-
 export default App;
