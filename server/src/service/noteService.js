3cfcc779d17c061765587afacc64ac966c1ce95a
Cycbloom <1756138655@qq.com>
2025-03-30 14:39:00 +0800

    refactor(server-note): 将笔记标签处理逻辑移至tagService以提升代码复用性
    将noteService中处理笔记标签的逻辑提取到tagService中，包括标签的创建、删除和查询功能。

--- server/src/service/noteService.js
+++ server/src/service/noteService.js
@@ -1,30 +1,18 @@
 const NoteModel = require("../models/NoteModel");
-const NoteTagModel = require("../models/NoteTagModel");
-const TagModel = require("../models/TagModel");
+const tagService = require("./tagService");
 
 class NoteService {
   async createNote(noteData) {
     const { tags, ...noteDataWithoutTags } = noteData;
     const note = await NoteModel.create(noteDataWithoutTags);
-    let tagObjects = [];
-    if (tags && tags.length > 0) {
-      // 将标签名转换为标签对象（包含id）
-      tagObjects = await Promise.all(
-        tags.map((tagName) => TagModel.findOrCreateByName(tagName))
-      );
-      // 创建笔记和标签的关联
-      await Promise.all(
-        tagObjects.map((tag) => NoteTagModel.addTagToNote(note.id, tag.id))
-      );
-    }
-
-    return { ...note, tags: tags };
+    const noteTags = await tagService.handleNoteTags(note.id, tags);
+    return { ...note, tags: noteTags };
   }
 
   async getNoteById(id) {
     const note = await NoteModel.findById(id);
     if (note) {
-      const tags = await NoteTagModel.getTagsByNoteId(note.id);
+      const tags = await tagService.getNoteTagsByNoteId(note.id);
       return { ...note, tags };
     }
     return null;
@@ -39,7 +27,7 @@ class NoteService {
     // 为每个笔记添加标签信息
     const notesWithTags = await Promise.all(
       notes.map(async (note) => {
-        const tags = await NoteTagModel.getTagsByNoteId(note.id);
+        const tags = await tagService.getNoteTagsByNoteId(note.id);
         return { ...note, tags };
       })
     );
@@ -51,29 +39,15 @@ class NoteService {
     const updatedNote = await NoteModel.update(id, noteDataWithoutTags);
 
     if (updatedNote) {
-      let tagObjects = [];
-      if (tags && tags.length > 0) {
-        // 将标签名转换为标签对象（包含id）
-        tagObjects = await Promise.all(
-          tags.map((tagName) => TagModel.findOrCreateByName(tagName))
-        );
-        // 先删除原有标签关联，再创建新的关联
-        await NoteTagModel.deleteNoteAllTags(id);
-        await Promise.all(
-          tagObjects.map((tag) => NoteTagModel.addTagToNote(id, tag.id))
-        );
-      } else if (tags) {
-        // 如果tags是空数组，只需要删除原有标签关联
-        await NoteTagModel.deleteNoteAllTags(id);
-      }
-      return { ...updatedNote, tags: tagObjects };
+      const noteTags = await tagService.handleNoteTags(id, tags);
+      return { ...updatedNote, tags: noteTags };
     }
     return null;
   }
 
   async deleteNote(id) {
     // 先删除笔记相关的所有标签关联
-    await NoteTagModel.deleteNoteAllTags(id);
+    await tagService.handleNoteTags(id, []);
     // 再删除笔记本身
     return await NoteModel.delete(id);
   }
--- server/src/service/tagService.js
+++ server/src/service/tagService.js
@@ -1,4 +1,5 @@
 const TagModel = require("../models/TagModel");
+const NoteTagModel = require("../models/NoteTagModel");
 
 // 获取所有标签
 const getAllTags = async (query = {}) => {
@@ -25,10 +26,37 @@ const deleteTag = async (id) => {
   return await TagModel.delete(id);
 };
 
+// 处理笔记的标签
+const handleNoteTags = async (noteId, tags) => {
+  let tagObjects = [];
+  if (tags && tags.length > 0) {
+    // 将标签名转换为标签对象（包含id）
+    tagObjects = await Promise.all(
+      tags.map((tagName) => TagModel.findOrCreateByName(tagName))
+    );
+    // 先删除原有标签关联，再创建新的关联
+    await NoteTagModel.deleteNoteAllTags(noteId);
+    await Promise.all(
+      tagObjects.map((tag) => NoteTagModel.addTagToNote(noteId, tag.id))
+    );
+  } else if (tags) {
+    // 如果tags是空数组，只需要删除原有标签关联
+    await NoteTagModel.deleteNoteAllTags(noteId);
+  }
+  return tags;
+};
+
+// 获取笔记的标签
+const getNoteTagsByNoteId = async (noteId) => {
+  return await NoteTagModel.getTagsByNoteId(noteId);
+};
+
 module.exports = {
   getAllTags,
   getTagById,
   createTag,
   updateTag,
   deleteTag,
+  handleNoteTags,
+  getNoteTagsByNoteId,
 };
